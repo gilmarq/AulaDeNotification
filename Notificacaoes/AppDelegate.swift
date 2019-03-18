d09b896de21331045b6a86f334eebf58cd90ca8c
Usuário Convidado <Guest@pa06micro06.local>
2019-03-18 20:35:29 -0300

    criação do projeto

--- Notificacaoes/AppDelegate.swift
+++ Notificacaoes/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import
+UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let center = UNUserNotificationCenter.current()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+         center.delegate = self
+        
         return true
     }
 
@@ -42,5 +46,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+extension AppDelegate :UNUserNotificationCenterDelegate{
+    
 }
 
--- Notificacaoes/ViewController.swift
+++ Notificacaoes/ViewController.swift
@@ -7,14 +7,43 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfCourse: UITextField!
+    @IBOutlet weak var tfSubject: UITextField!
+    @IBOutlet weak var dpDate: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func scheduleNotification(_ sender: UIButton) {
+        let content = UNMutableNotificationContent()
+        content.title = "Lembrete"
+        content.subtitle = "Curso: \(tfCourse.text!)"
+        content.body = "Está na hora de estudar \(tfSubject.text!)"
+        content.badge = 1
+        content.sound = UNNotificationSound.default
+        content.categoryIdentifier = "Lembrete"
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: "Lembrete", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error{
+                print("Erro na notificação:", error)
+            }
+            
+        }
+        
+        
+        
+        
+        
+    }
+    
 }
 
